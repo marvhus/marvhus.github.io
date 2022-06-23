b6f70e4b66ef3972adfa3325222f6f8608e34086
marvhus <44480334+marvhus@users.noreply.github.com>
2022-06-23 23:48:04 +0200

    made website and desktop styles

--- js/loadScript.js
+++
@@ -1,8 +0,0 @@
-function loadScript(url)
-{    
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    head.appendChild(script);
-}
